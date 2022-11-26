6a709e15903e08547898f1f86b29f358c77a0294
Logan <logan.nelson012@gmail.com>
2022-11-25 16:05:58 -0800

    Created four working math functions using a and b for the variables.

---
+++ calc.js
@@ -0,0 +1,24 @@
+/* The Math!! */
+
+const add = function(a , b){
+    return a+b;
+}
+console.log(add);
+
+const subtract = function(a , b) {
+    return a - b;
+}
+console.log(subtract);
+
+const divide = function(a , b) {
+    return a / b;
+}
+console.log(divide);
+
+const multiply = function(a , b) {
+    return a * b;
+}
+console.log(multiply);
+
+/* Operator Function */
+
