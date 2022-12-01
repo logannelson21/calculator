63da694a851e9ddf0cf3570a1980d386250454d2
Logan <logan.nelson012@gmail.com>
2022-11-30 21:57:28 -0800

    Made event listeners for button pushes on the calculator.

--- calc.js
+++ calc.js
@@ -22,10 +22,10 @@ console.log(multiply);
 
 /* Operator Function */
 
-function operate(operater , a , b) {
+function operate(operator , a , b) {
     a = Number(a);
     b = Number(b);
-    switch (operater) {
+    switch (operator) {
         case '+':
             return add(a , b);
         case '-':
@@ -39,4 +39,54 @@ function operate(operater , a , b) {
             return null;                     
     }
 
+}
+
+/* Display Functions */
+
+let operator = '';
+let inputValue = '';
+let currentValue = '';
+
+document.addEventListener('DOMContentLoaded' , function() {
+    let clear = document.querySelector('.clr');    
+    let decimal = document.querySelector('#decimal');
+    let dlt = document.querySelector('.dlt');
+
+    let numbers = document.querySelectorAll('.number')
+    let operators = document.querySelectorAll('.operator');
+
+    let inputScreen = document.querySelector('.input');
+    let currentScreen = document.querySelector('.current');
+
+    numbers.forEach((number) => number.addEventListener('click' , function(e){
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue;
+    }))
+
+    operators.forEach((op) => op.addEventListener('click' , function(e){
+        handleOperator(e.target.textContent)
+        inputScreen.textContent = inputValue + '' + operator;
+
+    }))
+
+    clear.addEventListener('click' , function(){
+        inputValue = '';
+        currentValue = '';
+        operator = '';
+        inputScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
+})
+
+
+function handleNumber(num) {
+    if(currentValue.length <= 5){
+        currentValue += num;
+    }
+}
+
+function handleOperator(op) {
+    operator = op;
+    inputValue = currentValue;
+    currentValue = '';
 }
